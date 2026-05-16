8938e080f827c6c8ff64a3843f51cdbf17115fe7
pupkpupk7-jpg <pupkpupk7@gmail.com>
2026-05-16 14:47:13 +0500

    Update MainHook.java

--- app/src/main/java/com/devicespooflab/hooks/MainHook.java
+++ app/src/main/java/com/devicespooflab/hooks/MainHook.java
@@ -50,32 +50,11 @@ public class MainHook implements IXposedHookLoadPackage {
         // Log that we're loading for this package
 
         if ("com.google.android.gms".equals(lpparam.packageName)) {
-            // 1. Сразу отсекаем процесс аттестации
+    
             if (lpparam.processName != null && lpparam.processName.contains("unstable")) {
-                XposedBridge.log(TAG + "UNSTABLE");
-                return;
-             }
-
-            boolean isTargetProcess = lpparam.processName != null && (lpparam.processName.contains("prime") || lpparam.processName.contains("persistent"));
-
-            // Если это один из этих двух процессов — только тогда включаем фильтр по Биндеру
-            if (isTargetProcess) {
-                int callingUid = Binder.getCallingUid();
-                int myUid = Process.myUid();
-            
-                // Если процесс GMS вызывает свойства сам для себя — подмену не делаем
-                if (callingUid == myUid) return;
-
-                String callingPackage = getPackageNameForUid(callingUid);
-
-                // КРИТИЧЕСКИЙ ФИЛЬТР: Если вызов идет НЕ от Google One (red) — 
-                // делаем жесткий return, прерывая хук. Пойдут родные свойства устройства.
-                if (callingPackage == null || !callingPackage.equals("com.google.android.apps.subscriptions.red")) {
-                    XposedBridge.log(TAG + "NO RED");
-                    return; 
+                XposedBridge.log(TAG + " UNSTABLE - BLOCK SPOOF TO SAVE INTEGRITY");
+            return; 
                 }
-            }
-            
         }
 
         XposedBridge.log(TAG + ": Loading hooks for " + lpparam.packageName);
